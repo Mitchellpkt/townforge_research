94b2745c289b6da1e13d689ec63009ceb33aa8d0
Mitchell Krawiec-Thayer <contact@mitchellpkt.com>
2022-08-28 12:49:02 -0600

    chore: initial analysis (length histograms)

--- analysis/read_files.py
+++ analysis/read_files.py
@@ -1,18 +1,26 @@
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
+
+import pandas as pd
 
 
-def read_transctions(
+def read_transactions_to_dataframe(
     top_data_directory: Path = Path.cwd() / ".." / "data" / "unpacked",
-) -> List[str]:
+) -> pd.DataFrame:
     # Get the file paths
     all_paths: List[Path] = list(top_data_directory.glob("**/*"))
     file_paths: List[Path] = [f for f in all_paths if f.is_file()]
 
     # Read in the files
-    transactions: List[str] = []
+    transactions: List[Tuple[str, str]] = []
     for file_path in file_paths:
         with open(file_path, "r") as f:
-            transactions.append(f.read())
+            txn_text: str = f.read().replace("\n", "")  # remove newline
+            transactions.append((txn_text, str(file_path)))
 
-    return transactions
+    return pd.DataFrame(
+        {
+            "transaction": list(zip(*transactions))[0],
+            "file": list(zip(*transactions))[1],
+        }
+    )
